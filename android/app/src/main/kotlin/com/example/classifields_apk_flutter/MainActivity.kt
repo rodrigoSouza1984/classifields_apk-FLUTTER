9bb1196484748e9a1281ec310ad7b5400a946e30
rodrigoSouza1984 <apk_vini_rod@hotmail.com>
2023-05-26 15:05:08 -0300

    first commit initial and confi start projetct

---
+++ android/app/src/main/kotlin/com/example/classifields_apk_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.classifields_apk_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:classifields_apk_flutter/src/pages/signIn/sign_in_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SignInScreen(),
+    );
+  }
+}
+
+
